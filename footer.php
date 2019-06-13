dbfd265d098fcde2835570be159ba2fa08810782
Riley Cleary <rileykeycleary@yahoo.com>
2019-06-13 12:06:09 -0600

    Adds submenu, address, phonenumber, and copyright to footer

--- footer.php
+++ footer.php
@@ -13,12 +13,31 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer navbar sticky-bottom">
-		<div class="site-info d-flex flex-row">
+	<footer id="colophon" class="site-footer">
+		<div class="site-info d-flex flex-row justify-content-center">
 			<span class="text-uppercase align-self-center">Join our email list</span>
 			<input class="align-self-center" type="text" name="email" value="">
-			<div class="text-uppercase text-center align-self-center">Put me on <br />the list!</div>
+			<a class="text-uppercase text-center align-self-center" href="#" >Put me on <br />the list!</a>
 		</div><!-- .site-info -->
+		<div class="footer-submenu-section" >
+			<div class="wrapper">
+				<div class="main-submenu d-flex flex-row justify-content-center">
+					<a href="#">Calendar</a>
+					<span>|</span>
+					<a href="#">Employment</a>
+					<span>|</span>
+					<a href="#">Contact</a>
+					<span>|</span>
+					<a href="#">Suggestion Box</a>
+					<span>|</span>
+					<a href="#">Media</a>
+				</div>
+				<div id="address" class="text-center">
+					<p class="text-uppercase">620 G St. Davis. California 95616 <span>|</span> (530) 758-2667 <span>|</span> Open Daily 7am – 10pm</p>
+				</div>
+				<div class="copyright text-center">	&copy; <?php echo date("Y"); ?> Davis Food Co-op. All rights reserved.</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
