2c6272df99a042b7ec199247860ec775544c8e3b
Riley Cleary <rileykeycleary@yahoo.com>
2019-06-15 10:37:35 -0600

    Adds front page template and adds bottom image row place holder to front page

---
+++ front-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying front page
+ *
+ * @package Seven-Roots-Davis-Coop
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+            <h1>Front page</h1>
+            <div class="container p-0">
+                <div class="placehold-image">
+                </div>
+                <div class="placehold-image">
+                </div>
+                <div class="placehold-image">
+                </div>
+                <div class="placehold-image">
+                </div>
+                <div class="placehold-image">
+                </div>
+            </div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
