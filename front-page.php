e469e0f51e961d77775426731866639d3b6e26d6
Riley Cleary <rileykeycleary@yahoo.com>
2019-06-15 11:32:25 -0600

    Adds styles for 'whats new' section

--- front-page.php
+++ front-page.php
@@ -13,10 +13,33 @@ get_header();
             <?php
                 $seven_roots_davis_coop_description = get_bloginfo( 'description', 'display' );
                 if ( $seven_roots_davis_coop_description || is_customize_preview() ) :
-			?>
+            ?>
+            <div class="whats-new container clearfix">
+                <div class="whats-new-item">
+                    <div class="whats-new-placehold">
+                    </div>
+                    <div class="whats-new-title text-center">
+                        <a href="#" class="text-uppercase">Local Farms</a>
+                    </div>
+                </div>
+                <div class="whats-new-item">
+                    <div class="whats-new-placehold">
+                    </div>
+                    <div class="whats-new-title text-center">
+                        <a href="#" class="text-uppercase">On Sale Now</a>
+                    </div>                    
+                </div>
+                <div class="whats-new-item">
+                    <div class="whats-new-placehold">
+                    </div>
+                    <div class="whats-new-title text-center">
+                        <a href="#" class="text-uppercase">Classes + Events</a>
+                    </div>                   
+                </div>
+            </div>
 			<p class="site-description"><?php echo $seven_roots_davis_coop_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
-            <div class="container p-0">
+            <div class="container p-0 clearfix featured-image-row">
                 <div class="placehold-image">
                 </div>
                 <div class="placehold-image">
