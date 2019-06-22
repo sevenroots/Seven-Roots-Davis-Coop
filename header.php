3a543988cf8c205358c18f30d817ca3f975a5d24
Riley Cleary <rileykeycleary@yahoo.com>
2019-06-21 18:47:15 -0600

    Styles for main header nav items complete, still needs social media links

--- header.php
+++ header.php
@@ -25,30 +25,24 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'seven-roots-davis-coop' ); ?></a>
 
 	<header id="masthead" class="site-header container-fluid">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<div class="container d-flex">
+			<div class="site-branding">
 				<?php
-			else :
+				the_custom_logo();
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			</div><!-- .site-branding -->
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'seven-roots-davis-coop' ); ?></button>
 				<?php
-			endif;
-			?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'seven-roots-davis-coop' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+					'container'		 => false,
+					'menu_class'	 => 'menu nav-menu clearfix',
+				) );
+				?>
+			</nav><!-- #site-navigation -->
+		</div><!-- .container -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content container">
